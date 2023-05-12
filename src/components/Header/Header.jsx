31bdcbc258c7a388b516d60fb877a9c69797a4a9
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-05-12 20:53:57 +0600

    delete button set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
   return (
-    <nav>
+    <nav className="header">
       <Link to="/">Home</Link>
       <Link to="/review">Order Review</Link>
       <Link to="/about">About</Link>
